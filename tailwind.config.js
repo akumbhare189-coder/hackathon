76d054687e75cad89bf789d962e29b7133c2f353
Ayush Kumbhare <akumbhare189@gmail.com>
2026-02-14 11:15:23 +0530

    Add Tailwind CSS configuration with custom theme colors for Cyber Shield platform

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/**
+ * Tailwind CSS Configuration File
+ *
+ * This file is used to configure the Tailwind CSS framework.
+ */
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'deep-blue': '#0A1F44',
+        'safety-orange': '#FF6B00',
+        'white': '#FFFFFF',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
