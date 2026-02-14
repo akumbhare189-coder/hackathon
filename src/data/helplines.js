372af4d32fbd9bd6c10c3c5419e534fe3ad9827d
Ayush Kumbhare <akumbhare189@gmail.com>
2026-02-14 11:15:28 +0530

    Add emergency helpline numbers including Cyber Crime Helpline and National Cyber Crime Portal.

---
+++ src/data/helplines.js
@@ -0,0 +1,7 @@
+// Emergency Helpline Numbers
+const helplines = {
+    "Cyber Crime Helpline": "1930",
+    "National Cyber Crime Portal": "https://cybercrime.gov.in"
+};
+
+module.exports = helplines;
